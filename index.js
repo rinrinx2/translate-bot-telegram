4069044acf4a2dc7eeefbcedbebb428ce02afadb
Frangky <81846311+Franky404@users.noreply.github.com>
2021-09-06 23:48:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,22 @@
 *  Repo: https://github.com/Franky404/translate-bot-telegram
 */
 //==============================================//
-// settings 
-const fs = require('fs-extra');
-const config = JSON.parse(fs.readFileSync('./config.json'));
 // Module
 const { Telegraf } = require('telegraf');
-const  LocalSession  = require('telegraf-session-local');
 const bot = new Telegraf(config.TELEGRAM_BOT_TOKEN);
-const axios = require('axios');
 const translate = require('@vitalets/google-translate-api')
-// session
-bot.use(new LocalSession({ database: '.data/session.json' }).middleware());
 // start
-bot.start(ctx => ctx.reply('Welcome👋🏻'));
+bot.start(ctx => ctx.reply('Welcome 👋🏻,Silahkan Gunakan Bot Translate Ke Bahasa English Dengan Baik:)'));
 // message 
 bot.on('message', ctx => {
 const lang = 'en'
 translate(`${ctx.message.text}`,{to:`${lang}`}).then( res => {
      const translation = res.text
       ctx.reply(translation);
-      if (ctx.session.dnt === true) {
-        return;
-      }
-      let messages = JSON.parse(ctx.session.messages) || [];
-      messages.push({ text: ctx.message.text, translation: translation });
-      ctx.session.messages = JSON.stringify(messages);
     });
 });
 //started
 bot.startPolling();
+/*
+* Simple Bot By Franky :)
+*/
